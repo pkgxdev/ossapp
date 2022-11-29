00886bac5e874dc108ae81195cf49cc6d82964a1
neil <neil@neils-MacBook-Pro.local>
2022-11-29 13:56:15 +0800

    #39 partial implementation

--- packages/gui/src/components/PackageBanner/PackageBanner.svelte
+++ packages/gui/src/components/PackageBanner/PackageBanner.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
 	import '$appcss';
 	import '@tea/ui/icons/icons.css';
-	import Placeholder from '$components/Placeholder/Placeholder.svelte';
+	import type { Package } from '@tea/ui/types';
+
+	export let pkg: Package;
 </script>
 
-<Placeholder label="PackageBanner" />
+<section class="border-gray border bg-black">
+	<header class="flex p-2">
+		<figure class="w-1/3 grow-1">
+			<img width={260} src={pkg.thumb_image_url} alt={pkg.full_name} />
+		</figure>
+		<article class="w-2/3 p-4">
+			<h3 class="text-primary">{pkg.full_name}</h3>
+			{#if pkg.maintainer}
+				<h3>* {pkg.maintainer}</h3>
+			{/if}
+			<span>stars</span>
+			<p>{pkg.desc}</p>
+		</article>
+	</header>
+	<footer class="flex text-white">
+		<div>sh &lt;(curl tea.xyz ) +textin</div>
+		<div>copy</div>
+		<div>open terminal</div>
+	</footer>
+</section>
