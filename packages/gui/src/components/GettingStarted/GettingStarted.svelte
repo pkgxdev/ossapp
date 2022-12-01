4e82622828b89272c6e04acbb251b17b0564cedd
neil <neil@neils-MacBook-Pro.local>
2022-12-01 10:02:37 +0800

    #34 GettingStarted gui component * added @tea/ui/ArticleCard component too

--- packages/gui/src/components/GettingStarted/GettingStarted.svelte
+++ packages/gui/src/components/GettingStarted/GettingStarted.svelte
@@ -1,6 +1,45 @@
 <script lang="ts">
 	import '$appcss';
-	import Placeholder from '$components/Placeholder/Placeholder.svelte';
+	import ArticleCard from '@tea/ui/ArticleCard/ArticleCard.svelte';
+
+	const doStuff = () => {
+		console.log('do stuff!');
+	};
 </script>
 
-<Placeholder label="GettingStarted a" />
+<header class="text-primary border border-gray p-4">GETTING STARTED WITH TEA</header>
+<section class="grid grid-cols-3">
+	<div class="border border-gray p-4">
+		<ArticleCard
+			content={{
+				title: 'installing tea',
+				copy: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptatum molestiae esse quisquam earum debitis.',
+				img_url: '/images/bored-ape.png',
+				cta_label: 'Get Started',
+				link: '/cli'
+			}}
+		/>
+	</div>
+	<div class="border border-gray p-4">
+		<ArticleCard
+			content={{
+				title: 'authenticating',
+				copy: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptatum molestiae esse quisquam earum debitis.',
+				img_url: '/images/bored-ape.png',
+				cta_label: 'Get Started',
+				link: ''
+			}}
+			onClick={doStuff}
+		/>
+	</div>
+	<div class="border border-gray p-4">
+		<ArticleCard
+			content={{
+				title: 'give us a star',
+				copy: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptatum molestiae esse quisquam earum debitis.',
+				img_url: '/images/bored-ape.png',
+				cta_label: 'Get Started'
+			}}
+		/>
+	</div>
+</section>
---
+++ packages/ui/src/ArticleCard/ArticleCard.stories.ts
@@ -0,0 +1,35 @@
+import ArticleCard from './ArticleCard.svelte';
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/svelte/writing-stories/introduction
+export default {
+	title: 'Example/ArticleCard',
+	component: ArticleCard,
+	tags: ['docsPage'],
+	render: ({ content }) => ({
+		Component: ArticleCard,
+		props: { content }
+	}),
+	argTypes: {
+		content: {
+			name: 'content',
+			description: 'this is type Article'
+		},
+		onClick: {
+			name: 'onClick',
+			description: 'this is optional function'
+		}
+	}
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/7.0/svelte/writing-stories/args
+export const Example = {
+	args: {
+		content: {
+			title: 'installing tea',
+			copy: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptatum molestiae esse quisquam earum debitis.',
+			img_url: 'https://tea.xyz/Images/packages/unicode_org.jpg',
+			cta_label: 'Get Started',
+			link: '/cli'
+		}
+	}
+};
