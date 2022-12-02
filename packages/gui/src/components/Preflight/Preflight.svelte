56b46370304bdab9ba5a2ae887cd1a60c42d5ffa
Thomas Smith <49042513+tsmitty11@users.noreply.github.com>
2022-12-01 19:18:33 -0500

    Initial content

--- packages/gui/src/components/Preflight/Preflight.svelte
+++ packages/gui/src/components/Preflight/Preflight.svelte
@@ -3,4 +3,28 @@
 	import Placeholder from '$components/Placeholder/Placeholder.svelte';
 </script>
 
-<Placeholder label="Preflight" />
+<div class="card social-box" style="width: 100%; float:right;">
+	<header class="text-primary border-b border-gray p-5">PRE-FLIGHT</header>
+	<div class="listbox-item border-b border-gray p-5">
+		<p>Install Tea</p>
+	</div>
+	<div class="listbox-item border-b border-gray p-5">
+		<p>Authenticate</p>
+	</div>
+	<div class="listbox-item p-5">
+		<p>Give tea a star</p>
+	</div>
+</div>
+
+<style>
+
+.card{
+	border: 2px solid #949494;
+	background-color: #1a1a1a;
+}
+
+.listbox-item{
+	height: 75px;
+}
+
+</style>
