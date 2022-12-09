199397548d543e62a8af27afc8b2602b5dc89915
neil <neil@neils-MacBook-Pro.local>
2022-12-09 09:43:30 +0800

    #70 use airtable data by GET /posts?tag=news/course

--- packages/gui/src/components/EssentialWorkshops/EssentialWorkshops.svelte
+++ packages/gui/src/components/EssentialWorkshops/EssentialWorkshops.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import '$appcss';
-	import { getAllPosts } from '$libs/api/mock';
+	import { getAllPosts } from '@api';
 	import type { AirtablePost } from '@tea/ui/types';
 	import Posts from '@tea/ui/Posts/Posts.svelte';
 	import PanelHeader from '@tea/ui/PanelHeader/PanelHeader.svelte';
+	import Preloader from '@tea/ui/Preloader/Preloader.svelte';
 	import { onMount } from 'svelte';
 
 	let courses: AirtablePost[] = [];
@@ -14,4 +15,10 @@
 </script>
 
 <PanelHeader title="Essential Workshops" ctaLabel="View all" ctaLink="/" />
-<Posts posts={courses} />
+{#if courses.length}
+	<Posts posts={courses} />
+{:else}
+	<section class="h-64 border border-gray p-4">
+		<Preloader />
+	</section>
+{/if}
