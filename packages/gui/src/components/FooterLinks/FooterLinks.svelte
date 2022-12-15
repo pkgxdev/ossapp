ea9d79765950d47a7fe9a172331422ed9a762ea6
Thomas Smith <49042513+tsmitty11@users.noreply.github.com>
2022-12-15 17:29:51 -0500

    Initial footer work

--- packages/gui/src/components/FooterLinks/FooterLinks.svelte
+++ packages/gui/src/components/FooterLinks/FooterLinks.svelte
@@ -1,8 +1,16 @@
 <footer class="h-autofont-machina relative w-full bg-black">
-	<section class="h-56 p-4 px-16">
-		<a class="text-xl text-primary" href="/"> Quick Links (TBD) </a>
-	</section>
+	<section class="h-56 p-4 px-16" />
 	<section class="h-16 border border-r-0 border-gray p-4 px-16">
-		<a class="text-sm text-primary" href="/"> terms and services </a>
+		<div class="footer flex">
+			<div class="about w-1/2">
+				<h3>Changing the way you build. And what happens after you build it.</h3>
+				<p>
+					tea.cli is a delightful package manager that gets out of your way and sets you up for a
+					joyful development process. You can install it today. Our web3 protocol will create an OSS
+					ecosystem that is safer for all users, and fair for all maintainers. Read our white paper
+					here.
+				</p>
+			</div>
+		</div>
 	</section>
 </footer>
