97a288c90f33d64dbcaa49986d46ba46ebd41305
Thomas Smith <49042513+tsmitty11@users.noreply.github.com>
2022-12-15 20:58:32 -0500

    Content

--- packages/gui/src/components/FooterLinks/FooterLinks.svelte
+++ packages/gui/src/components/FooterLinks/FooterLinks.svelte
@@ -1,9 +1,10 @@
 <footer class="h-autofont-machina relative w-full bg-black">
-	<section class="h-56 p-4 px-16" />
-	<section class="h-16 border border-r-0 border-gray p-4 px-16">
-		<div class="footer flex">
-			<div class="about w-1/2">
-				<h3>Changing the way you build. And what happens after you build it.</h3>
+	<section class="p-4 px-16 py-16">
+		<div class="flex">
+			<div class="footer-text w-3/4 pr-16">
+				<h3 class="mb-5 text-4xl text-primary">
+					Changing the way you build. And what happens after you build it.
+				</h3>
 				<p>
 					tea.cli is a delightful package manager that gets out of your way and sets you up for a
 					joyful development process. You can install it today. Our web3 protocol will create an OSS
@@ -11,6 +12,18 @@
 					here.
 				</p>
 			</div>
+
+			<div class="socials w-1/4">
+				<ul class="h-full border-2 border-gray">
+					<li class="h-1/5 border-b border-gray px-3 py-1">Facebook</li>
+					<li class="h-1/5 border-b border-gray px-3 py-1">Facebook</li>
+					<li class="h-1/5 border-b border-gray px-3 py-1">Facebook</li>
+					<li class="h-1/5 border-b border-gray px-3 py-1">Facebook</li>
+					<li class="h-1/5 border-b border-gray px-3 py-1">Facebook</li>
+				</ul>
+			</div>
 		</div>
 	</section>
+
+	<section class="h-16 border border-r-0 border-gray p-4 px-16" />
 </footer>
