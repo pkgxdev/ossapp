bc9ea6e6c271ad6b6eba5a45f4ff67e67aac66ea
neil <neil@neils-MacBook-Pro.local>
2023-01-09 11:28:56 +0800

    #127 reposition traffic lights

--- modules/gui/src-tauri/src/main.rs
+++ modules/gui/src-tauri/src/main.rs
@@ -5,11 +5,25 @@
 
 mod handlers;
 
+#[cfg(target_os = "macos")]
+#[macro_use]
+extern crate objc;
+
+use tauri::{Manager, WindowEvent};
+use window_ext::WindowExt;
+mod window_ext;
+
 fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![
       handlers::packages::install_package,
     ])
+    .setup(|app| {
+        let win = app.get_window("main").unwrap();
+        // win.set_transparent_titlebar(true);
+        // win.position_traffic_lights(30.0, 30.0);
+        Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
