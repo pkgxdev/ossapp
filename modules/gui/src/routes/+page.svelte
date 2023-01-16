1fc9c9dc08fbc4e7a421f05551621bd437b9cf2a
neil <neil@neils-MacBook-Pro.local>
2023-01-16 14:38:01 +0800

    #146 AssetType in home page

--- modules/gui/src/routes/+page.svelte
+++ modules/gui/src/routes/+page.svelte
@@ -5,13 +5,40 @@
 	import EssentialWorkshops from '$components/EssentialWorkshops/EssentialWorkshops.svelte';
 	import Packages from '$components/Packages/Packages.svelte';
 	import News from '$components/News/News.svelte';
-	import CategorizedPackages from '$components/CategorizedPackages/CategorizedPackages.svelte';
+
+	import HeaderCard from '@tea/ui/HeaderCard/HeaderCard.svelte';
+	import ListAction from '@tea/ui/ListAction/ListAction.svelte';
 </script>
 
 <div>
 	<PageHeader coverUrl="/images/headers/header_bg_1.png">Discover</PageHeader>
 	<section class="mt-8 mb-8">
 		<Packages title="FOUNDATIN ESSENTIALS" />
+	</section>
+	<PageHeader coverUrl="/images/headers/header_bg_1.png">ASSET TYPE</PageHeader>
+	<section class="mt-8 mb-8 flex gap-4">
+		<div>
+			<HeaderCard
+				title="Browser Packages"
+				imgUrl="/images/bored-ape.png"
+				ctaUrl="/packages"
+				ctaLabel="Browse packages >"
+				articleTitle="What are packages?"
+				description="Collections of files aggregated to form larger frameworks & functions. Think Python or Node.js."
+			/>
+			<ListAction title="Top packages this week" mainCtaTitle="VIEW ALL PACKAGES" />
+		</div>
+		<div>
+			<HeaderCard
+				title="Browse Scripts"
+				imgUrl="/images/bored-ape.png"
+				ctaUrl="/packages"
+				ctaLabel="Browse scripts >"
+				articleTitle="What are scripts?"
+				description="Invisible applications that chain packages together in order to perform cool actions on your computer."
+			/>
+			<ListAction title="Top scripts this week" mainCtaTitle="VIEW ALL SCRIPTS" />
+		</div>
 	</section>
 	<PageHeader coverUrl="/images/headers/header_bg_1.png">TUTORIALS</PageHeader>
 	<section class="mt-8 mb-8">
