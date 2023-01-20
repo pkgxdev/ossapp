84c3ce2b2d0ebd93cb4d533e9d6b50b8afa243b3
Jacob Heider <jacob@tea.xyz>
2023-01-20 17:34:39 -0500

    better trigger change

--- modules/gui/src-tauri/src/main.rs
+++ modules/gui/src-tauri/src/main.rs
@@ -1,15 +1,15 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 mod handlers;
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![
-      handlers::packages::install_package,
-    ])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            handlers::packages::install_package,
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
