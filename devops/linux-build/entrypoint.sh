be504522dd7a8cab550f24dfdf7cc784730676be
neil <neil@neils-MacBook-Pro.local>
2023-01-24 20:28:52 +0800

    workspace

--- devops/linux-build/entrypoint.sh
+++ devops/linux-build/entrypoint.sh
@@ -1,14 +1,3 @@
 #!/bin/sh -l
 
-
-ls ~/node_modules || true
-ls ~/.pnpm-store || true
-ls ~/.cargo || true
-ls ~/modules/gui/src-tauri/target || true
-
 tea -SE xc build
-
-ls ~/node_modules || true
-ls ~/.pnpm-store || true
-ls ~/.cargo || true
-ls ~/modules/gui/src-tauri/target || true
