6261df57360cc9d6692566d3c4536a2b0f8357fb
neil <neil@neils-MacBook-Pro.local>
2023-01-25 19:12:48 +0800

    rm custo action

--- modules/gui/src/components/Badges/Badges.svelte
+++ modules/gui/src/components/Badges/Badges.svelte
@@ -2,8 +2,6 @@
 	import '$appcss';
 	import Placeholder from '$components/Placeholder/Placeholder.svelte';
 
-	export let arg1: string;
 </script>
 
 <Placeholder label="Badges" />
-<h1>{arg1 || 'tes'}</h1>
