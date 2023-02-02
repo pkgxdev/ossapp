65d56af0966d5e2506c9bb9dbce5c28e71887aa9
neil <neil@neils-MacBook-Pro.local>
2023-02-02 11:53:11 +0800

    lint

--- modules/gui/tailwind.config.ts
+++ modules/gui/tailwind.config.ts
@@ -1,5 +1,4 @@
-/** @type {import('tailwindcss').Config} */
-const { theme, plugins } = require('@tea/ui/tailwind.config.cjs');
+import { theme, plugins } from '@tea/ui/tailwind.config.cjs';
 module.exports = {
 	content: ['./src/**/*.{html,svelte,ts,js}', '../ui/src/**/*.{html,svelte,ts,js}'],
 	theme,
