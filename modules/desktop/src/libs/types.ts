7562581563d7ee31c9f8e060e466c3f7ed6623db
neil <neil@neils-MacBook-Pro.local>
2023-02-08 09:52:23 +0800

    run

--- modules/desktop/src/libs/types.ts
+++ modules/desktop/src/libs/types.ts
@@ -1,4 +1,4 @@
-// as much possible add types here that are unique to @tea/gui use only
+// as much possible add types here that are unique to @tea/desktop use only
 // else
 // 		please use the package @tea/ui/src/types.ts
 //		things that go there are shared types/shapes like ie: Package
