32fa59bd3bc78b3262eaf77035580ef1bc790499
neil <neil@neils-MacBook-Pro.local>
2023-02-16 15:32:33 +0800

    undo electro-builder notarization

--- modules/desktop/electron-builder.config.cjs
+++ modules/desktop/electron-builder.config.cjs
@@ -1,6 +1,6 @@
-const { notarize } = require('@electron/notarize');
-const fs = require('fs');
-const path = require('path');
+// const { notarize } = require('@electron/notarize');
+// const fs = require('fs');
+// const path = require('path');
 
 module.exports = {
 	appId: "xyz.tea.gui",
@@ -10,41 +10,41 @@ module.exports = {
 	files: ["electron/dist/electron.cjs", { from: "build", to: "" }],
 	// TODO: if xcrun altool exists eventually in our self-hosted macos
 	// SOLUTION: is notarize separately in next pipeline step
-	afterSign: async (params) => {
-		if (process.platform !== 'darwin') {
-			return;
-		}
+	// afterSign: async (params) => {
+	// 	if (process.platform !== 'darwin') {
+	// 		return;
+	// 	}
 
-		console.log('afterSign hook triggered', params);
+	// 	console.log('afterSign hook triggered', params);
 
-		const appBundleId = 'xyz.tea.gui';
+	// 	const appBundleId = 'xyz.tea.gui';
 
-		let appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
-		if (!fs.existsSync(appPath)) {
-			console.log('skip');
-			return;
-		}
+	// 	let appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
+	// 	if (!fs.existsSync(appPath)) {
+	// 		console.log('skip');
+	// 		return;
+	// 	}
 
-		console.log(`Notarizing ${appBundleId} found at ${appPath} with Apple ID ${process.env.APPLE_ID}`);
+	// 	console.log(`Notarizing ${appBundleId} found at ${appPath} with Apple ID ${process.env.APPLE_ID}`);
 
-		try {
-			await notarize({
-				appBundleId,
-				appPath,
-				appleId: process.env.APPLE_ID,
-				appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD
-			});
-		} catch (error) {
-			console.error(error);
-		}
+	// 	try {
+	// 		await notarize({
+	// 			appBundleId,
+	// 			appPath,
+	// 			appleId: process.env.APPLE_ID,
+	// 			appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD
+	// 		});
+	// 	} catch (error) {
+	// 		console.error(error);
+	// 	}
 
-		console.log(`Done notarizing`);
-	},
+	// 	console.log(`Done notarizing`);
+	// },
 
-	publish: {
-		provider: "github",
-		repo: "gui",
-		owner: "teaxyz",
-		releaseType: "release"
-	},
+	// publish: {
+	// 	provider: "github",
+	// 	repo: "gui",
+	// 	owner: "teaxyz",
+	// 	releaseType: "release"
+	// },
 };
