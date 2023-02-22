e5fd744c3fdfa8b699b13ab2d364148c5c60510c
Neil <get.neilmolina@gmail.com>
2023-02-22 10:55:30 +0800

    #220 make reusable build-sign-notarize jobs (#221)
    * #220 make reusable build-sign-notarize jobs
    
    * #220 update main and release to reuse build-sign job
    
    ---------
    
    Co-authored-by: neil <neil@neils-MacBook-Pro.local>

--- modules/desktop/src/components/Badges/Badges.svelte
+++ modules/desktop/src/components/Badges/Badges.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
 	import '$appcss';
-	import Placeholder from '$components/Placeholder/Placeholder.svelte';
-
-	export let arg1: string;
+	import Placeholder from '$components/placeholder/placeholder.svelte';
 </script>
 
 <Placeholder label="Badges" />
-<h1>{arg1 || 'tes'}</h1>
