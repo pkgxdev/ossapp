77eb2e2f8707bf40397b10ec8177471adb15a9c3
ABevier <awbevier@gmail.com>
2023-03-03 12:36:20 -0500

    styling changes

--- modules/desktop/src/components/tea-update/tea-update.svelte
+++ modules/desktop/src/components/tea-update/tea-update.svelte
@@ -43,16 +43,16 @@
 </script>
 
 {#if updateState === 'not_installed'}
-<div class="banner flex items-center justify-between text-primary">
-    {$t('package.install-tea-cli')}
-    <Button class="w-16 border-1 text-sm" onClick={onOpenTerminal}>{$t("package.install-label")}</Button>
+<div class="banner flex items-center justify-center text-primary">
+    <span class="text-white mr-4">{$t('package.install-tea-cli')}</span>
+	<button class="button" on:click={onOpenTerminal}>{$t("package.install-tea-label")}</button>
 </div>
 {/if}
 
 {#if updateState === 'update_required'}
-<div class="banner flex items-center justify-between text-primary">
-	{$t('package.update-tea-cli')} 
-    <Button class="w-16 border-1 text-sm" onClick={onOpenTerminal}>{$t("package.update-label")}</Button>
+<div class="banner flex items-center justify-center text-primary">
+	<span class="text-white mr-4">{$t('package.update-tea-cli')}</span>
+    <button class="button" on:click={onOpenTerminal}>{$t("package.update-tea-label")}</button>
 </div>
 {/if}
 
@@ -64,5 +64,38 @@
 		padding: 6px;
 		z-index: 1;
 		position: relative;
+		padding-top: 12px;
+		padding-bottom: 12px;
+	}
+
+	.button {
+		color: white;
+		border: 1px solid white;
+		padding: 6px 24px;
+		position: relative;
+	}
+
+	.button::before {
+		position: absolute;
+		content: "";
+		background: #e1e1e1;
+		transition-duration: 0.2s;
+		z-index: -1;
+		inset: 0px auto auto 0px;
+		width: 0px;
+		height: 100%;
+		opacity: 1;
+	}
+
+	.button:hover::before {
+		width: 100%;
+		height: 100%;
+		opacity: 1;
+	}
+
+	.button:hover {
+		color: #1a1a1a;
+		background: #e1e1e1;
+		transition: color 0.3s ease 0s, background 0s ease 0.3s;
 	}
 </style>
