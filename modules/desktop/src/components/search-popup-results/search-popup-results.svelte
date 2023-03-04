e94c316436bde3a847afb7e44910e59b8581edd5
neil <neil@neils-MacBook-Pro.local>
2023-03-04 16:32:52 +0800

    #256 change homepage to packages list page

--- modules/desktop/src/components/search-popup-results/search-popup-results.svelte
+++ modules/desktop/src/components/search-popup-results/search-popup-results.svelte
@@ -4,14 +4,14 @@
 	import Preloader from '@tea/ui/Preloader/Preloader.svelte';
 	import Package from "$components/packages/package.svelte";
 	import { PackageStates } from '$libs/types';
-	import Posts from '@tea/ui/posts/posts.svelte';
+	// import Posts from '@tea/ui/posts/posts.svelte';
 
 	import { installPackage } from '@native';
 	import type { AirtablePost } from '@tea/ui/types';
 	let term: string;
 	let packages: GUIPackage[] = [];
-	let articles: AirtablePost[] = []; // news, blogs, etc
-	let workshops: AirtablePost[] = []; // workshops, course
+	// let articles: AirtablePost[] = []; // news, blogs, etc
+	// let workshops: AirtablePost[] = []; // workshops, course
 	let loading = true;
 
 	searchStore.subscribe((v) => {
@@ -20,21 +20,21 @@
 	searchStore.packagesSearch.subscribe((pkgs) => {
 		packages = pkgs;
 	});
-	searchStore.postsSearch.subscribe((posts) => {
-		let partialArticles: AirtablePost[] = [];
-		let partialWorkshops: AirtablePost[] = [];
-		for (let post of posts) {
-			if (post.tags.includes('news')) {
-				partialArticles.push(post);
-			}
-			if (post.tags.includes('course') || post.tags.includes('featured_course')) {
-				partialWorkshops.push(post);
-			}
-		}
+	// searchStore.postsSearch.subscribe((posts) => {
+	// 	let partialArticles: AirtablePost[] = [];
+	// 	let partialWorkshops: AirtablePost[] = [];
+	// 	for (let post of posts) {
+	// 		if (post.tags.includes('news')) {
+	// 			partialArticles.push(post);
+	// 		}
+	// 		if (post.tags.includes('course') || post.tags.includes('featured_course')) {
+	// 			partialWorkshops.push(post);
+	// 		}
+	// 	}
 
-		articles = partialArticles;
-		workshops = partialWorkshops;
-	});
+	// 	articles = partialArticles;
+	// 	workshops = partialWorkshops;
+	// });
 
 	searchStore.searching.subscribe((v) => (loading = v));
 
@@ -54,8 +54,8 @@
 		</header>
 		<menu class="bg-accent flex h-8 w-full gap-4 px-4 text-xs">
 			<button>packages ({packages.length})</button>
-			<button>articles ({articles.length})</button>
-			<button>workshops ({workshops.length})</button>
+			<!-- <button>articles ({articles.length})</button>
+			<button>workshops ({workshops.length})</button> -->
 		</menu>
 		<header class="text-primary p-4 text-lg">
 			Top Package Results ({packages.length})
@@ -86,7 +86,7 @@
 				{/each}
 			{/if}
 		</ul>
-		<header class="text-primary p-4 text-lg">
+		<!-- <header class="text-primary p-4 text-lg">
 			Top Article Results ({articles.length})
 		</header>
 		{#if articles.length}
@@ -105,7 +105,7 @@
 			<section class="border-gray h-64 border bg-black p-4">
 				<Preloader />
 			</section>
-		{/if}
+		{/if} -->
 	</div>
 </section>
 
