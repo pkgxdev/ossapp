4384280d2741f7c91c89dd1416ca65df3eb993ae
neil <neil@neils-MacBook-Pro.local>
2023-03-07 08:27:16 +0800

    #264 i18n dropdown ui

--- modules/desktop/src/components/select-lang/select-lang.svelte
+++ modules/desktop/src/components/select-lang/select-lang.svelte
@@ -2,8 +2,11 @@
   import { t, locales, locale } from '$libs/translations'; 
 </script>
 
-<select bind:value="{$locale}" class="bg-black text-sm text-gray">
-  {#each $locales as value}
-    <option value="{value}">{$t(`lang.${value}`)}</option>
-  {/each}
-</select>
+<section class="flex items-center border border-gray h-8 mt-1 mr-1 px-2 w-48 rounded-sm">
+  <i class="icon-world text-gray mt-2"></i>
+  <select bind:value="{$locale}" class="bg-black text-sm text-gray flex-grow">
+    {#each $locales as value}
+      <option value="{value}">{$t(`lang.${value}`)}</option>
+    {/each}
+  </select>
+</section>
