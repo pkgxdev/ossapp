db84f9c7f1b4d0670bc674fc3d9d11d2280e7b99
Neil <get.neilmolina@gmail.com>
2023-03-08 14:15:42 +0800

    Package Progress install (#272)
    * #263 fake progress for package installation
    
    ---------
    
    Co-authored-by: neil <neil@neils-MacBook-Pro.local>

--- modules/desktop/src/components/packages/package.svelte
+++ modules/desktop/src/components/packages/package.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
 	import '$appcss';
 	import PackageCard from '@tea/ui/package-card/package-card.svelte';
+
 	import { t } from '$libs/translations';
 	import type { GUIPackage } from '$libs/types';
   import { PackageStates } from '$libs/types';
+  import { getPackage } from '@native';
+  import { inc } from 'semver';
 
 
 	export let pkg: GUIPackage;
   export let onClick: () => void;
+
+	let fakeLoadingProgress = 0;
 
 	const getCTALabel = (state: PackageStates): string => {
 		return {
@@ -18,11 +23,39 @@
 			[PackageStates.NEEDS_UPDATE]: $t("package.needs-update-label").toUpperCase(),
 		}[state];
 	};
+
+	let fakeTimer: NodeJS.Timer;
+
+	async function startFakeLoader() {
+		let ms = 100;
+		let assumedDlSpeedMb = 1024 * 1024 * 3; // 3mbps
+		const p = await getPackage(pkg.full_name);
+		const size = (p?.bottles?.length ? p.bottles[0].bytes : assumedDlSpeedMb*10);
+		const eta = size / assumedDlSpeedMb;
+
+		const increment = (1 / eta)/10;
+
+		fakeTimer = setInterval(() => {
+			const progressLeft = 100 - fakeLoadingProgress;
+			const addProgress = progressLeft * increment;
+			fakeLoadingProgress = fakeLoadingProgress + addProgress;
+		}, ms);
+	}
 </script>
+
 
 <PackageCard
   {pkg}
   link={`/packages/${pkg.slug}`}
   ctaLabel={getCTALabel(pkg.state)}
-  onClickCTA={onClick}
+	progessLoading={+fakeLoadingProgress.toFixed(2)}
+  onClickCTA={async () => {
+		fakeLoadingProgress = 1;
+		startFakeLoader();
+		await onClick();
+		setTimeout(() => {
+			clearTimeout(fakeTimer);
+			fakeLoadingProgress = 100;
+		}, 1500);
+	}}
 />
