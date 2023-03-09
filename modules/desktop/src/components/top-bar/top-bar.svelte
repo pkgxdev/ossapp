9b71a7813a470ac96aa9caf2b39cccff95b88dd1
neil <neil@neils-MacBook-Pro.local>
2023-03-09 11:59:56 +0800

    #127 adjust spacing for next back buttons

--- modules/desktop/src/components/top-bar/top-bar.svelte
+++ modules/desktop/src/components/top-bar/top-bar.svelte
@@ -22,15 +22,17 @@
 </script>
 
 <header class="border-gray flex w-full border border-l-0 border-r-0" style="-webkit-app-region: drag">
-	<div class="w-16">
+	<div class="w-16 mr-2">
 		<!-- just spacing for the close/expand/ buttons in title bar
 			todo: handle this different when on linux probably move to right
 		-->
 	</div>
-	<ul class="text-gray flex h-10 gap-4 pl-4 align-middle leading-10">
-		<button on:click={navStore.back} class={$prevPath ? 'active' : ''}>&#8592</button>
-		<button on:click={navStore.next} class={$nextPath ? 'active' : ''}>&#8594</button>
-	</ul>
+	{#if $prevPath || $nextPath}
+		<ul class="text-gray flex h-10 gap-4 pl-2 align-middle leading-10 mr-2">
+			<button on:click={navStore.back} class={$prevPath ? 'active' : ''}>&#8592</button>
+			<button on:click={navStore.next} class={$nextPath ? 'active' : ''}>&#8594</button>
+		</ul>
+	{/if}
 	<div class="p-1 flex-grow h-8">
 		<SearchInput
 			class="w-full border border-gray rounded-sm"
