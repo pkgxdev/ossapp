16c5b76ae6b6a1717288db035babbbddac61b7c6
neil molina <neil@neils-MacBook-Pro.local>
2023-03-24 13:38:34 +0800

    #333 new top-bar

--- modules/desktop/src/components/top-bar/profile-nav-button.svelte
+++ modules/desktop/src/components/top-bar/profile-nav-button.svelte
@@ -15,8 +15,11 @@
 
 <button class="flex items-center justify-center py-2" on:click={toggleSideNav}>
 	{#if $user}
-		<figure class="flex rounded-full overflow-clip w-{8} h-{8} mr-1">
-			<img src={$user.avatar_url || '/images/bored-ape.png'} alt="profile" />
+		<figure class="flex gap-2 h-{8} mr-1 border border-gray rounded-md items-center px-1">
+			<div class="text-gray font-inter bg-black">@{$user.login}</div>
+			<div class="overflow-clip w-{6} h-{6} rounded-sm">
+				<img class="w-{6} w-{6} h-{6}" src={$user.avatar_url || '/images/bored-ape.png'} alt="profile" />
+			</div>
 		</figure>
 	{:else}
 		<div class="icon-hamburger-icon-square text-xl mt-2 py-4 px-2 text-gray"></div>
--- modules/desktop/src/components/top-bar/top-bar.svelte
+++ modules/desktop/src/components/top-bar/top-bar.svelte
@@ -21,19 +21,15 @@
 	});
 </script>
 
-<header class="border-gray flex items-center w-full border border-l-0 border-r-0 h-12" style="-webkit-app-region: drag">
-	<div class="w-16 mr-2">
-		<!-- just spacing for the traffice-lights buttons in title bar
-			todo: handle this different when on linux probably move to right
-		-->
-	</div>
-	{#if $prevPath || $nextPath}
-		<ul class="text-gray flex h-10 gap-4 pl-2 align-middle leading-10 mr-2">
-			<button on:click={navStore.back} class={$prevPath ? 'active' : ''}>&#8592</button>
-			<button on:click={navStore.next} class={$nextPath ? 'active' : ''}>&#8594</button>
-		</ul>
-	{/if}
-	<div class="px-2 flex-grow">
+<header class="border-gray flex items-center justify-between border w-full h-12 pr-2 rounded-t-xl" style="-webkit-app-region: drag">
+	<ul class="text-gray flex h-10 gap-2 align-middle leading-10 pl-20">
+		<a href="/">
+			<button id="home" class="text-xl hover:bg-primary w-12 rounded-md text-center"><i class="icon-tea-logo-iconasset-1"/></button>
+		</a>
+		<button on:click={navStore.back} class={$prevPath ? 'active' : 'opacity-50'}>&#8592</button>
+		<button on:click={navStore.next} class={$nextPath ? 'active' : 'opacity-50'}>&#8594</button>
+	</ul>
+	<div class="px-2 w-1/3">
 		<SearchInput
 			class="w-full border border-gray rounded-sm h-9"
 			size="small"
@@ -52,8 +48,13 @@
 	header {
 		background: rgba(26,26,26,0.9);
 		backdrop-filter: blur(2px);
+		box-sizing: border-box;
 	}
-
+	#home {
+		height: 40px;
+		width: 40px;
+		background: #222222;
+	}
 	ul button {
 		pointer-events: none;
 	}
