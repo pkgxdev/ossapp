c2c050e9d7f1661e3539e298984ee3682b9e1dad
neil molina <neil@neils-MacBook-Pro.local>
2023-03-28 12:52:18 +0800

    #345 home button hover effect

--- modules/desktop/src/components/top-bar/top-bar.svelte
+++ modules/desktop/src/components/top-bar/top-bar.svelte
@@ -23,7 +23,7 @@
 <header class="border-gray flex items-center justify-between border w-full h-12 pr-2 rounded-t-xl" style="-webkit-app-region: drag">
 	<ul class="text-gray flex h-10 gap-2 align-middle leading-10 pl-20">
 		<a href="/">
-			<button id="home" class="text-xl hover:bg-primary w-12 rounded-md text-center"><i class="icon-tea-logo-iconasset-1"/></button>
+			<div class="home-btn transition-all text-xl w-12 rounded-md text-center"><i class="icon-tea-logo-iconasset-1"/></div>
 		</a>
 		<button on:click={navStore.back} class={$prevPath ? 'active' : 'opacity-50'}>&#8592</button>
 		<button on:click={navStore.next} class={$nextPath ? 'active' : 'opacity-50'}>&#8594</button>
@@ -49,13 +49,23 @@
 		backdrop-filter: blur(2px);
 		box-sizing: border-box;
 	}
-	#home {
+	.home-btn {
 		height: 40px;
 		width: 40px;
-		background: #222222;
+		line-height: 40px;
+		padding-left: 3px;
+		background-color: rgba(34, 34, 34, 1);
+		cursor: auto;
+	}
+	.home-btn:hover {
+		background-size: cover;
+		background-position: center center;
+		background-image: url("/images/gradient-bg.png");
+		color: #222222;
 	}
 	ul button {
 		pointer-events: none;
+		cursor: auto;
 	}
 
 	ul button.active {
