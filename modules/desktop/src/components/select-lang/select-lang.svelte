bf84495377d2a98685d6cc8149007b45bd86dc41
Neil <get.neilmolina@gmail.com>
2023-04-05 13:26:06 +0800

    #389 select language dropdown (#394)
    Co-authored-by: neil molina <neil@neils-MacBook-Pro.local>

--- modules/desktop/src/components/select-lang/select-lang.svelte
+++ modules/desktop/src/components/select-lang/select-lang.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
   import { t, locales, locale } from '$libs/translations'; 
+  import SelectExpand from "@tea/ui/select-expand/select-expand.svelte";
+
+  const label = "Language";
 </script>
 
-<section class="flex h-8 mt-1 w-full hover:bg-gray hover:border">
-  <select bind:value="{$locale}" class="bg-black hover:bg-gray text-sm text-white flex-grow">
-    {#each $locales as value}
-      <option value="{value}">{$t(`lang.${value}`)}</option>
-    {/each}
-  </select>
-</section>
+<SelectExpand
+  {label}
+  bind:value={$locale}
+  options={$locales.map((value) => ({
+    label: $t(`lang.${value}`),
+    value,
+    selected: value === $locale,
+  }))}
+/>
