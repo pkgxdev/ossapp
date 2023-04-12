f00dbb9f733a2d921604a0e82a155e44bbb3e629
Neil <get.neilmolina@gmail.com>
2023-04-12 14:12:50 +0800

    #441 add new pushy app id (#442)
    Co-authored-by: neil molina <neil@neils-MacBook-Pro.local>

--- modules/desktop/electron/libs/push-notification.ts
+++ modules/desktop/electron/libs/push-notification.ts
@@ -14,7 +14,7 @@ import { app } from "electron";
 export default function initialize(mainWindow: BrowserWindow) {
 	Pushy.listen();
 	// Register device for push notifications
-	Pushy.register({ appId: "64110fb47446e48a2a0e906d" })
+	Pushy.register({ appId: "643647948f3b62fb34b29989" })
 		.then(async (push_token) => {
 			const { device_id } = await readSessionData();
 			if (device_id) await post(`/auth/device/${device_id}/register-push-token`, { push_token });
