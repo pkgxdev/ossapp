94c875b64b929631e63709223e1f590270d26cc4
neil molina <neil@neils-MacBook-Pro.local>
2023-04-14 17:08:09 +0800

    copy file working

--- modules/desktop/electron/libs/initialize.ts
+++ modules/desktop/electron/libs/initialize.ts
@@ -1,9 +1,11 @@
 import fs from "fs";
+import { app } from "electron";
 import { getGuiPath } from "./tea-dir";
 import * as log from "electron-log";
 import semver from "semver";
 import { cliBinPath, asyncExec } from "./cli";
 import { createInitialSessionFile } from "./auth";
+import path from "path";
 
 const destinationDirectory = getGuiPath();
 
@@ -21,15 +23,27 @@ export async function initializeTeaCli(): Promise<string> {
 			fs.mkdirSync(destinationDirectory, { recursive: true });
 		}
 
-		const curlCommand = `curl -L -o "${cliBinPath}" "${binaryUrl}"`;
-
 		if (fs.existsSync(cliBinPath)) {
 			log.info("binary tea already exists at", cliBinPath);
 			binCheck = await asyncExec(`cd ${destinationDirectory} && ./tea --version`);
 		} else {
-			try {
+			// /Applications/tea.app/Contents/Resources/app.asar
+			const appPath = app.getAppPath();
+			if (appPath.endsWith(".asar")) {
+				const arch = (process.arch as string) === "arm64" ? "arm64" : "x86-64";
+				const unpackedDir = appPath.replace(".asar", ".asar.unpacked");
+				const binFile = `tea-${arch}`;
+				const cpBin = path.join(unpackedDir, "external", binFile);
+				await asyncExec(`cp ${cpBin} ${cliBinPath}`);
+				log.info("Binary copied and saved to", cliBinPath);
+			} else {
+				// downlaod binary remotely
+				const curlCommand = `curl -L -o "${cliBinPath}" "${binaryUrl}"`;
 				await asyncExec(curlCommand);
 				log.info("Binary downloaded and saved to", cliBinPath);
+			}
+
+			try {
 				await asyncExec("chmod u+x " + cliBinPath);
 				log.info("Binary is now ready for use at", cliBinPath);
 				binCheck = await asyncExec(`cd ${destinationDirectory} && ./tea --version`);
