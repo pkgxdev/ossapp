bf176a643e8f5946c0df8d79742e1c729dd0b747
neil molina <neil@neils-MacBook-Pro.local>
2023-04-25 10:50:55 +0800

    rm log

--- modules/desktop/src/libs/logger.ts
+++ modules/desktop/src/libs/logger.ts
@@ -1,11 +1,10 @@
 const log = window ? window.require("electron-log") : console;
 import { captureException } from "./sentry";
 
-// TODO: figure out how to detect if packaged
+// TODO: figure out how to detect if pkaged
 const oldError = log.error;
 log.error = (...params: any[]) => {
 	oldError(params);
 	captureException(params[0].message);
 };
-log.info("not working");
 export default log;
