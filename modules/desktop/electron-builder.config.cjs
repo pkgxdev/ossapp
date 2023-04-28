f4a93f45dde631cf0bb8138810986cfe38212421
Max Howell <mxcl@me.com>
2023-04-28 07:19:30 -0400

    wip

--- modules/desktop/electron-builder.config.cjs
+++ modules/desktop/electron-builder.config.cjs
@@ -1,9 +1,3 @@
-const { notarize } = require("@electron/notarize");
-const fs = require("fs");
-const path = require("path");
-const otaClient = require("@crowdin/ota-client");
-const _ = require("lodash");
-
 module.exports = {
   appId: "xyz.tea.gui",
   productName: "tea",
@@ -19,39 +13,6 @@ module.exports = {
       target: "default",
       arch: ["x64", "arm64"]
     }
-  },
-  afterSign: async (params) => {
-    if (process.platform !== "darwin" || process.env.CSC_IDENTITY_AUTO_DISCOVERY === "false") {
-      console.log("not notarizing app");
-      return;
-    }
-
-    console.log("afterSign hook triggered");
-
-    const appBundleId = "xyz.tea.gui";
-
-    let appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
-    if (!fs.existsSync(appPath)) {
-      console.log("skip");
-      return;
-    }
-
-    console.log(
-      `Notarizing ${appBundleId} found at ${appPath} with Apple ID ${process.env.APPLE_ID}`
-    );
-
-    try {
-      await notarize({
-        appBundleId,
-        appPath,
-        appleId: process.env.APPLE_ID,
-        appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD
-      });
-    } catch (error) {
-      console.error(error);
-    }
-
-    console.log(`Done notarizing`);
   },
   // this determines the configuration of the auto-update feature
   publish: {
