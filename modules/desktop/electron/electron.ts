e93a517b2e396b000dc1fd0242781253b83abb59
neil molina <neil@neils-MacBook-Pro.local>
2023-04-28 08:56:48 +0800

    add uid and username in main thread logs

--- modules/desktop/electron/electron.ts
+++ modules/desktop/electron/electron.ts
@@ -15,6 +15,7 @@ import initializePushNotification, {
 } from "./libs/push-notification";
 
 import init from "./libs/initialize";
+import { readSessionData } from "./libs/auth";
 
 log.info("App starting...");
 if (app.isPackaged) {
@@ -29,6 +30,13 @@ if (app.isPackaged) {
 				return ol ? "send" : "queue";
 			}
 		}
+	});
+	Sentry.configureScope(async (scope) => {
+		const session = await readSessionData();
+		scope.setUser({
+			id: session.device_id, // device_id this should exist in our pg db: developer_id is to many device_id
+			username: session?.user?.login || "" // github username or handler
+		});
 	});
 	setSentryLogging(Sentry);
 }
