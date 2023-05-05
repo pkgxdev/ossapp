0ea89ec4f9a582e493ddf9f11f481d4610af4c0e
ABevier <awbevier@gmail.com>
2023-05-04 20:57:13 -0400

    image blur effect escapes border (#558)

--- modules/desktop/src/components/package-card/bg-image.svelte
+++ modules/desktop/src/components/package-card/bg-image.svelte
@@ -60,7 +60,7 @@
   }
 </script>
 
-<section class="bg-black {clazz} {layout}">
+<section class="overflow-hidden bg-black {clazz} {layout}">
   <i
     class="logo icon-tea-logo-iconasset-1 text-3xl text-gray {layout}"
     class:animate-pulse={!pkg.thumb_image_url}
