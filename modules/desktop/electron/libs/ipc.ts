93c572c98f8b0395314fd0100893ef0dab01b95d
neil molina <neil@neils-MacBook-Pro.local>
2023-05-11 15:10:56 +0800

    add suggested strategy when calling quitAndInstall

--- modules/desktop/electron/libs/ipc.ts
+++ modules/desktop/electron/libs/ipc.ts
@@ -92,7 +92,9 @@ export default function initializeHandlers({ notifyMainWindow }: HandlerOptions)
     try {
       log.info("relaunching app");
       const autoUpdater = getUpdater();
-      await autoUpdater.quitAndInstall();
+      setImmediate(() => {
+        autoUpdater.quitAndInstall();
+      });
     } catch (error) {
       log.error(error);
     }
