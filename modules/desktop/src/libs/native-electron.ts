bb3ff23a186ec19523f9c059550cd6cfa8939c68
neil molina <neil@neils-MacBook-Pro.local>
2023-05-18 11:37:52 +0800

    use packages from postgres db

--- modules/desktop/src/libs/native-electron.ts
+++ modules/desktop/src/libs/native-electron.ts
@@ -26,7 +26,7 @@ const { ipcRenderer, shell } = window.require("electron");
 export async function getDistPackages(): Promise<Package[]> {
   try {
     return withRetry(async () => {
-      const req = await axios.get<Package[]>("https://gui.tea.xyz/packages.json");
+      const req = await axios.get<Package[]>("https://api.tea.xyz/v1/packages");
       log.info("packages received:", req.data.length);
       return req.data;
     });
