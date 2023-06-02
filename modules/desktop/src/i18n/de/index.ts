106bb03c5dd56fe1724a8a8af438d60aff736399
neil molina <get.neilmolina@gmail.com>
2023-06-02 10:02:28 +0800

    #641 initialize typesafe-i18n

---
+++ modules/desktop/src/i18n/de/index.ts
@@ -0,0 +1,9 @@
+import type { Translation } from "../i18n-types";
+
+const de = {
+  // this is an example Translation, just rename or delete this folder if you want
+  HI: "Hallo {name}! Bitte hinterlasse einen Stern, wenn dir das Projekt gefällt: https://github.com/ivanhofer/typesafe-i18n",
+  test: "Hallo Welt"
+} satisfies Translation;
+
+export default de;
