cb982a4ecb24c0f2542048faeba7435aaaf93f32
neil molina <get.neilmolina@gmail.com>
2023-07-20 12:10:30 +0800

    fix export

--- svelte/src/libs/stores.ts
+++ svelte/src/libs/stores.ts
@@ -4,18 +4,16 @@ import type { Package } from "$libs/types";
 
 import initAuthStore from "./stores/auth";
 import initNavStore from "./stores/nav";
-import pkgStore from "./stores/pkgs";
 import initNotificationStore from "./stores/notifications";
 import initAppUpdateStore from "./stores/update";
 import initScrollStore from "./stores/scroll";
 import ptysStore from "./stores/ptys";
-import * as search from "./stores/search";
+import pkgStore from "./stores/pkgs";
+export * as searchStore from "./stores/search";
 
 export const featuredPackages = writable<Package[]>([]);
 export const ptys = ptysStore;
 export const packagesStore = pkgStore;
-
-export const searchStore = search;
 
 export const authStore = initAuthStore();
 
