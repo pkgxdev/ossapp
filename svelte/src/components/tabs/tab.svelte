817b66b9b082bfea6faea88a85aa04cadc54e296
ABevier <awbevier@gmail.com>
2023-07-20 20:20:10 -0400

    new tab styling (#732)
    Co-authored-by: Neil <get.neilmolina@gmail.com>

--- svelte/src/components/tabs/tab.svelte
+++ svelte/src/components/tabs/tab.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <button
-  class="tab w-20 border-x"
+  class="tab w-20 border-x border-b"
   class:activetab={isActive}
   disabled={isDisabled}
   on:click={onClick}>{$t(label)}</button
@@ -16,18 +16,21 @@
 
 <style>
   .tab {
+    color: #ffffff;
     border-image: linear-gradient(to bottom, rgba(148, 148, 148, 0%), rgba(148, 148, 148, 100%)) 1;
-    background: linear-gradient(180deg, rgba(77, 77, 77, 0.7) 0%, rgba(77, 77, 77, 0.35) 100%);
   }
 
   .activetab {
     color: #00ffd0;
-    background: linear-gradient(180deg, rgba(77, 77, 77, 0.7) 0%, rgba(77, 77, 77, 0.35) 100%);
     border-image: linear-gradient(to bottom, rgba(0, 255, 208, 0%), rgba(0, 255, 208, 100%)) 1;
   }
 
   .tab:disabled {
     background: none;
-    background-color: #1a1a1a;
+    opacity: 0.3;
+  }
+
+  .tab:enabled:hover {
+    background: rgba(77, 77, 77, 0.35);
   }
 </style>
