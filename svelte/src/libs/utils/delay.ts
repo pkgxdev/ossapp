1b299005b115ec156d79b9d6c95517642ee88833
jiachen <ariesjia00@hotmail.com>
2023-11-09 23:50:27 +0800

    [test]: fix unit test command & add delay test

--- svelte/src/libs/utils/delay.ts
+++ svelte/src/libs/utils/delay.ts
@@ -1,5 +1,7 @@
 // withDelay adds a delay before calling the provided function.
+export const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 export default async function withDelay<T>(f: () => T, delayMs: number) {
-  await new Promise((resolve) => setTimeout(resolve, delayMs));
+  await wait(delayMs)
   return f();
 }
