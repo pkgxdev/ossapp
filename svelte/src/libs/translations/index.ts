6e3cbf96cfefe8360a3cac2ef235d56941cd31ce
nondevspell <156613281+nondevspell@users.noreply.github.com>
2024-01-18 14:32:59 +0200

    Update index.ts
    added estonian language index

--- svelte/src/libs/translations/index.ts
+++ svelte/src/libs/translations/index.ts
@@ -20,6 +20,8 @@ import no from "./languages/no.json";
 import hr from "./languages/hr.json";
 import ja from "./languages/ja.json";
 import lt from "./languages/lt.json";
+import ee from "./languages/ee.json";
+
 
 import * as customModifiers from "./modifiers";
 
@@ -30,7 +32,7 @@ type Translation = typeof en.translations & {
 };
 
 // add new language json object here
-const languages = [en, zh, de, ru, uk, ptbr, pl, cz, lv, ro, it, se, tr, no, hr, ja, lt];
+const languages = [en, zh, de, ru, uk, ptbr, pl, cz, lv, ro, it, se, tr, no, hr, ja, lt, ee];
 
 const langs: { [lang: string]: string } = {};
 languages.forEach((lang) => {
