f3ecd27850bd66a6cc5f959b749b559c9c467183
conghcbapjp <conghc@bap.jp>
2024-02-27 01:02:38 +0700

    add Malay language

--- svelte/src/libs/translations/index.ts
+++ svelte/src/libs/translations/index.ts
@@ -32,6 +32,7 @@ import ie from "./languages/ie.json";
 import th from "./languages/th.json";
 import kz from "./languages/kz.json";
 import gr from "./languages/gr.json";
+import ms from "./languages/ms.json";
 
 import * as customModifiers from "./modifiers";
 
@@ -71,7 +72,8 @@ const languages = [
   ie,
   th,
   kz,
-  gr
+  gr,
+  ms
 ];
 
 const langs: { [lang: string]: string } = {};
