c7698bb9158ff1522b9307cae7f0467edff5f759
conghcbapjp <conghc@bap.jp>
2024-02-27 22:16:07 +0700

    fix conflict with id language

--- svelte/src/libs/translations/index.ts
+++ svelte/src/libs/translations/index.ts
@@ -34,6 +34,7 @@ import th from "./languages/th.json";
 import kz from "./languages/kz.json";
 import gr from "./languages/gr.json";
 import ms from "./languages/ms.json";
+import id from "./languages/id.json";
 
 import * as customModifiers from "./modifiers";
 
@@ -75,7 +76,8 @@ const languages = [
   th,
   kz,
   gr,
-  ms
+  ms,
+  id
 ];
 
 const langs: { [lang: string]: string } = {};
