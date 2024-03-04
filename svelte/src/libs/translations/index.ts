3114674ad43761bbfd6ee4061f2b57a48f8f3d20
Kurnia Misbah <116286924+kurniamisbah@users.noreply.github.com>
2024-03-04 20:43:42 +0700

    Update index.ts

--- svelte/src/libs/translations/index.ts
+++ svelte/src/libs/translations/index.ts
@@ -35,6 +35,7 @@ import kz from "./languages/kz.json";
 import gr from "./languages/gr.json";
 import ms from "./languages/ms.json";
 import id from "./languages/id.json";
+import sk from "./languages/sk.json";
 import ar from "./languages/ar.json";
 
 import * as customModifiers from "./modifiers";
@@ -79,6 +80,7 @@ const languages = [
   gr,
   ms,
   id
+  sk
   ar
 ];
 
