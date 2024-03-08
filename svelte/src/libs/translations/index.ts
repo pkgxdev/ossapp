85ea115c5e4c4098f01cb4e96bd55a1ad5b66d52
rineil <cuongpnv.qn91@gmail.com>
2024-03-08 09:22:19 +0700

    import lo lang to index

--- svelte/src/libs/translations/index.ts
+++ svelte/src/libs/translations/index.ts
@@ -37,6 +37,7 @@ import ms from "./languages/ms.json";
 import id from "./languages/id.json";
 import sk from "./languages/sk.json";
 import ee from "./languages/ee.json";
+import lo from "./languages/lo.json";
 
 import * as customModifiers from "./modifiers";
 
@@ -81,7 +82,8 @@ const languages = [
   ms,
   id,
   sk,
-  ee
+  ee,
+  lo
 ];
 
 const langs: { [lang: string]: string } = {};
