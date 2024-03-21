72d774553dc128d99dce9ba49862c6cf989dc072
RomarioEXT <109734150+RomarioEXT@users.noreply.github.com>
2024-03-21 19:06:51 +0600

    Update index.ts

--- svelte/src/libs/translations/index.ts
+++ svelte/src/libs/translations/index.ts
@@ -44,6 +44,7 @@ import srb from "./languages/srb.json";
 import is from "./languages/is.json";
 import lo from "./languages/lo.json";
 import dk from "./languages/dk.json";
+import ug from "./languages/ug.json";
 
 import * as customModifiers from "./modifiers";
 
@@ -95,7 +96,8 @@ const languages = [
   srb,
   is,
   lo,
-  dk
+  dk,
+  ug
 ];
 
 const langs: { [lang: string]: string } = {};
