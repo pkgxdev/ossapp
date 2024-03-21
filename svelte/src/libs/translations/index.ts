a6c827f62957bf922180a6b7e9ae7ebf087729df
RickyChez <109737490+RickyChez@users.noreply.github.com>
2024-03-21 19:14:04 +0600

    Update index.ts

--- svelte/src/libs/translations/index.ts
+++ svelte/src/libs/translations/index.ts
@@ -44,6 +44,7 @@ import srb from "./languages/srb.json";
 import is from "./languages/is.json";
 import lo from "./languages/lo.json";
 import dk from "./languages/dk.json";
+import mi from "./languages/mi.json";
 
 import * as customModifiers from "./modifiers";
 
@@ -95,7 +96,8 @@ const languages = [
   srb,
   is,
   lo,
-  dk
+  dk,
+  mi
 ];
 
 const langs: { [lang: string]: string } = {};
